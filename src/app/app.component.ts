9f3cc36c8f518622994efbc19d8ec015c67da90b
aylin <aylin.durur@derinarge.com>
2020-02-12 17:19:37 +0300

    custom storeState done! onContextMenuPreparing done! Footer summary added! Double selection summary field deleting is processing ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
-import {Component} from '@angular/core';
-import {Customer, Order, Service} from './app.service';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Order, Service} from './app.service';
+import {DxDataGridComponent} from 'devextreme-angular';
+
+export interface FooterSummaryColumn {
+  dataField: string;
+  type: 'sum' | 'min' | 'max' | 'avg' | 'count';
+}
 
 @Component({
   selector: 'app-root',
@@ -10,35 +16,92 @@ import {Customer, Order, Service} from './app.service';
 
 
 export class AppComponent {
+
+  // tslint:disable-next-line:max-line-length
+  gridStateRemote: string = '{"columns":[{"visibleIndex":1,"dataField":"OrderNumber","dataType":"number","width":130,"visible":true},{"visibleIndex":0,"dataField":"OrderDate","dataType":"date","visible":true,"sortOrder":"desc","sortIndex":0},{"visibleIndex":3,"dataField":"SaleAmount","dataType":"number","visible":true},{"visibleIndex":2,"dataField":"Employee","dataType":"string","visible":true},{"visibleIndex":4,"dataField":"CustomerStoreCity","dataType":"string","visible":true},{"visibleIndex":5,"dataField":"CustomerStoreState","dataType":"string","visible":true,"groupIndex":0}],"allowedPageSizes":[5,10,20],"filterPanel":{"filterEnabled":true},"filterValue":null,"searchText":"","pageIndex":0,"pageSize":20,"footerGroups":[{"dataField":"SaleAmount","type":"sum"},{"dataField":"SaleAmount","type":"count"},{"dataField":"CustomerStoreCity","type":"count"}]}';
+  gridState: any;
+
   orders: Order[];
-  customers: Customer[];
-  datafields: string [] = [];
+  summaryFields: FooterSummaryColumn[] = [];
+  @ViewChild('dataGrid', {static: false}) dataGrid: DxDataGridComponent;
 
   constructor(private service: Service) {
     this.orders = service.getOrders();
-    this.customers = service.getCustomers();
+
+    this.gridState = JSON.parse(this.gridStateRemote);
   }
 
-  SaveState = (state) => {
-    console.log(state);
+  loadState = () => {
+    console.log(this.gridState);
+    this.summaryFields = this.gridState.footerGroups;
+    return this.gridState;
+  };
+
+  test() {
+    const dataGridState: any = this.dataGrid.instance.state();
+    dataGridState.footerGroups = [...this.summaryFields];
+    console.log(JSON.stringify(dataGridState));
+  }
+
+  saveState = (state) => {
+    // console.log(state);
     console.log(JSON.stringify(state));
   };
 
-  addMenuItems(e) {
-    if (e.target === 'header') {
+  addMenuItems($event: any) {
+    if ($event.target === 'header') {
       // e.items can be undefined
-      if (!e.items) {
-        e.items = [];
+      if (!$event.items) {
+        $event.items = [];
       }
+      const menuItem = {
+        text: 'Footer',
+        items: [
+          {
+            text: 'Sum',
+            onItemClick: () => {
+              this.addOrRemoveSummaryFields($event.column.dataField, 'sum');
+            }
+          },
+          {
+            text: 'Count',
+            onItemClick: () => {
+              this.addOrRemoveSummaryFields($event.column.dataField, 'count');
+            }
+          },
+          {
+            text: 'Avg',
+            onItemClick: () => {
+              this.addOrRemoveSummaryFields($event.column.dataField, 'avg');
+            }
+          },
+          {
+            text: 'Min',
+            onItemClick: () => {
+              this.addOrRemoveSummaryFields($event.column.dataField, 'min');
+            }
+          },
+          {
+            text: 'Max',
+            onItemClick: () => {
+              this.addOrRemoveSummaryFields($event.column.dataField, 'max');
+            }
+          }
+        ]
+      };
+      $event.items.push(menuItem);
+    }
+  }
 
-      // Add a custom menu item
-      e.items.push({
-        text: 'Log Column Caption',
-        onItemClick: (dataField: string) => {
-          console.log(e.column.dataField);
-          this.datafields.push(e.column.dataField);
-        }
+  addOrRemoveSummaryFields(columnDataField: string, columnDataFieldType: 'sum' | 'min' | 'max' | 'avg' | 'count'): void {
+    const exists = this.summaryFields.find(f => f.dataField === columnDataField && f.type === columnDataFieldType);
+    if (!exists) {
+      this.summaryFields.push({
+        dataField: columnDataField,
+        type: columnDataFieldType
       });
+    } else {
+      console.log(this.summaryFields.filter(f => f.dataField === exists.dataField && f.type !== exists.type));
     }
   }
 }
